420a4df7ee3c4190694af01400aac323b1b4fd5c
flosell <florian.sellmayr@gmail.com>
2014-09-04 21:38:00 +0200

    now using async success-messages for manual trigger. work remains to immediately pass on the rest of the result to UI so we can actually display a trigger button there

--- src/lambdaci/manualtrigger.clj
+++ src/lambdaci/manualtrigger.clj
@@ -1,5 +1,6 @@
 (ns lambdaci.manualtrigger
-  (:require [lambdaci.dsl :as dsl]))
+  (:require [lambdaci.dsl :as dsl]
+            [clojure.core.async :as async]))
 
 (def ids-posted-to (atom #{}))
 
@@ -9,7 +10,15 @@
 (defn was-posted? [id]
   (contains? @ids-posted-to id))
 
+(defn wait-for-async [p]
+  (let [status-ch (async/chan 10)
+        result {:status status-ch}
+        waiting-future (future (dsl/wait-for p))]
+    (async/>!! status-ch :waiting)
+    (async/thread (async/>!! status-ch (:status @waiting-future)))
+    result))
+
 (defn wait-for-manual-trigger [& _]
   (let [id (str (java.util.UUID/randomUUID))]
-    (println "waititing for manual trigger, POST to id" id)
-    (dsl/wait-for #(was-posted? id))))
+    (println "waititing for manual trigger, POST to id" id) ;; try returning a result with status-channel and the id, see what the UI can do with it?
+    (wait-for-async #(was-posted? id))))
