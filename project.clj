ab52cbd88c096c664cdbcd903b104a9635fa7b5e
flosell <florian.sellmayr@gmail.com>
2014-09-07 17:01:59 +0200

    fixed license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject lambdacd "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.json "0.2.5"]
                  [me.raynes/conch "0.8.0"]
