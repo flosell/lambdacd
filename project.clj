db1e40f87943593cc417ebbd9ddaaf17ae1a5da2
flosell <florian.sellmayr@gmail.com>
2014-09-07 21:26:09 +0200

    a bit of metadata to make clojars happy

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lambdacd "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.flosell/lambdacd "0.1.0-alpha1"
+  :description "a library to create a continous delivery pipeline in code"
+  :url "http://github.com/flosell/lambdacd"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
