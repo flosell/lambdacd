d2f593d9a00b7f9a2e68cee8f0ccc7fc4e288a80
flosell <florian.sellmayr@gmail.com>
2014-09-07 17:00:29 +0200

    removed playground-file with remote-execution experiments. nice but I'm not working on that feature right now so it shouldn't clutter the codebase

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.nrepl "0.2.3"]
                  [org.clojure/data.json "0.2.5"]
                  [me.raynes/conch "0.8.0"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
