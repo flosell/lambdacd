558da6969cafe48c4b45eed93e2c94ee8ea426be
flosell <florian.sellmayr@gmail.com>
2014-09-08 23:06:16 +0200

    removing useless statements from test. no idea how in-parallel got into the steps for testing itself but now they are gone...

--- test/lambdacd/control_flow_test.clj
+++ test/lambdacd/control_flow_test.clj
@@ -39,7 +39,7 @@
   (testing "that one failing step fails the pipeline"
     (is (= {:outputs { [1 0 0] {:status :success} [2 0 0] {:status :failure}} :status :failure} ((in-parallel some-successful-step some-failing-step) {} [0 0]))))
   (testing "that it executes things faster than it would serially"
-    (is (close? 3 10 (my-time ((in-parallel some-step-taking-10ms in-parallel some-step-taking-10ms in-parallel some-step-taking-10ms) {} [0 0]))))))
+    (is (close? 3 10 (my-time ((in-parallel some-step-taking-10ms some-step-taking-10ms some-step-taking-10ms) {} [0 0]))))))
 
 (deftest in-cwd-test
   (testing "that it collects all the outputs together correctly and passes cwd to steps"
