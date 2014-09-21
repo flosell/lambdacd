d698336228887cf7a0fda200b98f745761877ff8
flosell <florian.sellmayr@gmail.com>
2014-09-21 15:15:19 +0200

    minor cleanup

--- src/lambdacd/core.clj
+++ src/lambdacd/core.clj
@@ -4,14 +4,12 @@
             [lambdacd.execution :as execution]
             [lambdacd.pipeline-state :as pipeline-state]))
 
-(defn initial-context []
-  {:_pipeline-state (atom pipeline-state/initial-pipeline-state)})
 
 (defn- start-pipeline-thread [pipeline-def context]
   (async/thread (while true (execution/run pipeline-def context))))
 
 (defn mk-pipeline [pipeline-def]
-  (let [context (initial-context)
-        state (:_pipeline-state context)]
+  (let [state (atom pipeline-state/initial-pipeline-state)
+        context {:_pipeline-state state}]
     {:ring-handler (server/ui-for pipeline-def state)
      :init (partial start-pipeline-thread pipeline-def context)}))
