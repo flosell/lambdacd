b98c912e9cca80e0769b75e39bb6f37223d403a9
flosell <florian.sellmayr@gmail.com>
2014-12-14 19:43:38 +0100

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdacd "0.1.0-alpha5"
+(defproject lambdacd "0.1.0-alpha6"
   :description "a library to create a continous delivery pipeline in code"
   :url "http://github.com/flosell/lambdacd"
   :license {:name "Apache License, version 2.0"
