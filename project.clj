116354054a6a95b3e3fb44d0c4f1cf2e391147ec
flosell <florian.sellmayr@gmail.com>
2015-02-01 17:08:59 +0100

    next development-snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdacd "0.1.0-alpha10"
+(defproject lambdacd "0.1.0-alpha11-SNAPSHOT"
   :description "a library to create a continous delivery pipeline in code"
   :url "http://github.com/flosell/lambdacd"
   :license {:name "Apache License, version 2.0"
