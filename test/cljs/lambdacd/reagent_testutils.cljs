82e3f2a6c86a671b7518170d2338634e8b7ca4c5
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-03-22 15:38:24 +0100

    clean up ui tests

---
+++ test/cljs/lambdacd/reagent_testutils.cljs
@@ -0,0 +1,20 @@
+(ns lambdacd.reagent-testutils
+  (:require [reagent.core :as reagent :refer [atom]]))
+
+(def isClient (not (nil? (try (.-document js/window)
+                              (catch js/Object e nil)))))
+
+(defn add-test-div [name]
+  (let [doc     js/document
+        body    (.-body js/document)
+        div     (.createElement doc "div")]
+    (.appendChild body div)
+    div))
+
+(defn with-mounted-component [comp f]
+  (when isClient
+    (let [div (add-test-div "_testreagent")]
+      (let [comp (reagent/render-component comp div #(f comp div))]
+        (reagent/unmount-component-at-node div)
+        (reagent/flush)
+        (.removeChild (.-body js/document) div)))))
--- test/cljs/lambdacd/ui_core_test.cljs
+++ test/cljs/lambdacd/ui_core_test.cljs
@@ -1,31 +1,9 @@
 (ns lambdacd.ui-core-test
   (:require [cemerick.cljs.test :refer-macros [is are deftest testing use-fixtures done]]
-            [reagent.core :as reagent :refer [atom]]
-            [lambdacd.ui-core :as core]))
+            [lambdacd.ui-core :as core]
+            [lambdacd.reagent-testutils :as r]))
 
-
-(def isClient (not (nil? (try (.-document js/window)
-                              (catch js/Object e nil)))))
-
-(def rflush reagent/flush)
-
-(defn add-test-div [name]
-  (let [doc     js/document
-        body    (.-body js/document)
-        div     (.createElement doc "div")]
-    (.appendChild body div)
-    div))
-
-(defn with-mounted-component [comp f]
-  (when isClient
-    (let [div (add-test-div "_testreagent")]
-      (let [comp (reagent/render-component comp div #(f comp div))]
-        (reagent/unmount-component-at-node div)
-        (reagent/flush)
-        (.removeChild (.-body js/document) div)))))
-
-
-(defn found-in [re div]
+(defn found-in [div re]
   (let [res (.-innerHTML div)]
     (if (re-find re res)
       true
@@ -34,8 +12,11 @@
 
 
 (deftest test-home
-         (with-mounted-component (core/build-history-component (atom [{:build-number 1} {:build-number 3}]))
-                                 (fn [c div]
-                                   (is (found-in #"Builds" div))
-                                   (is (found-in #"Build 1" div))
-                                   (is (found-in #"Build 3" div)))))
+  (r/with-mounted-component
+    (core/build-history-component
+      (atom [{:build-number 1} {:build-number 3}]))
+    (testing "that the history contains all the builds"
+      (fn [c div]
+        (is (found-in div #"Builds"))
+        (is (found-in div #"Build 1"))
+        (is (found-in div #"Build 3"))))))
