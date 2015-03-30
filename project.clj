657abb4a533fad69fa6f4eba9eb4bbbf99ec8ff0
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-03-30 22:19:55 +0200

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdacd "0.1.1"
+(defproject lambdacd "0.1.2-SNAPSHOT"
   :description "a library to create a continous delivery pipeline in code"
   :url "http://github.com/flosell/lambdacd"
   :license {:name "Apache License, version 2.0"
