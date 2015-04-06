49c920591c93e6ce85e04fe450a7a9facaa41850
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-04-06 17:06:30 +0200

    remove dead code, format code

--- src/clj/lambdacd/runners.clj
+++ src/clj/lambdacd/runners.clj
@@ -3,16 +3,12 @@
             [clojure.core.async :as async]
             [lambdacd.internal.pipeline-state :as pipeline-state]))
 
-(defn start-new-run-after-first-step-finished
-  ([{pipeline-def :pipeline-def context :context}]
-   (start-new-run-after-first-step-finished pipeline-def context))
-  ([pipeline-def context]
-   (let [pipeline-run-fn (fn [] (async/thread (execution/run pipeline-def context)))]
-     (pipeline-state/notify-when-most-recent-build-running context pipeline-run-fn)
-     (pipeline-run-fn))))
+(defn start-new-run-after-first-step-finished [{pipeline-def :pipeline-def context :context}]
+ (let [pipeline-run-fn (fn [] (async/thread (execution/run pipeline-def context)))]
+   (pipeline-state/notify-when-most-recent-build-running context pipeline-run-fn)
+   (pipeline-run-fn)))
 
-(defn start-one-run-after-another
-  ([{pipeline-def :pipeline-def context :context}]
-   (start-one-run-after-another pipeline-def context))
-  ([pipeline-def context]
-   (async/thread (while true (execution/run pipeline-def context)))))
+(defn start-one-run-after-another [{pipeline-def :pipeline-def context :context}]
+ (async/thread
+   (while true
+     (execution/run pipeline-def context))))
