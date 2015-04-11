3ea48afc07adb161162e734debf391eacc769caf
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-04-11 16:20:46 +0200

    add basic support for args and ctx in execute-until-failure

--- src/clj/lambdacd/steps/support.clj
+++ src/clj/lambdacd/steps/support.clj
@@ -1,21 +1,21 @@
 (ns lambdacd.steps.support)
 
 
-(defn execute-until-failure [fns]
+(defn execute-until-failure [args ctx fns]
   (loop [x (first fns)
          rest (rest fns)
          result {}]
     (if (nil? x)
       result
-      (let [step-result (x)
+      (let [step-result (x args ctx)
             complete-result (merge result step-result)]
         (if (not= :success (:status step-result))
           complete-result
           (recur (first rest) (next rest) complete-result))))))
 
 (defn to-fn [form]
-  `(fn [] ~form))
+  `(fn [args# ctx#] ~form))
 
 (defmacro chain [& forms]
   (let [fns (into [] (map to-fn forms))]
-    `(execute-until-failure ~fns)))
+    `(execute-until-failure {} {} ~fns)))
--- test/clj/lambdacd/steps/support_test.clj
+++ test/clj/lambdacd/steps/support_test.clj
@@ -10,8 +10,35 @@
 
 (defn some-failling-step [args ctx]
   {:status :failure})
+
+(defn some-step-returning-an-argument-passed-in [args ctx]
+  {:status :success :the-arg (:arg args)})
+
+(defn some-step-returning-the-context-passed-in [args ctx]
+  {:status :success :the-ctx-1 ctx})
+(defn some-other-step-returning-the-context-passed-in [args ctx]
+  {:status :success :the-ctx-2 ctx})
+
+
 (defn step-that-should-never-be-called [args ctx]
   (throw (IllegalStateException. "do not call me!")))
+
+(deftest execute-until-failure-test
+  (testing "that the input argument is passed to the first step"
+    (is (= {:status :success :the-arg 42} (execute-until-failure {:arg 42} {}
+                                                                 [some-step-returning-an-argument-passed-in]))))
+  (testing "that the results of two steps get merged"
+    (is (= {:status :success :foo :baz} (execute-until-failure {} {}
+                                                               [some-step some-other-step]))))
+  (testing "that a failing step stops the execution"
+    (is (= {:status :failure :foo :bar} (execute-until-failure {} {}
+                                                               [some-step
+                                                                some-failling-step
+                                                                step-that-should-never-be-called]))))
+  (testing "that the context is passed to all the steps"
+    (is (= {:status :success :the-ctx-1 {:v 42} :the-ctx-2 {:v 42}} (execute-until-failure {} {:v 42}
+                                                                   [some-step-returning-the-context-passed-in
+                                                                    some-other-step-returning-the-context-passed-in])))))
 
 (deftest chain-test
   (testing "that we can just call a single step"
@@ -20,10 +47,6 @@
     (is (= {:status :success :foo :baz} (chain
                                          (some-step {} {})
                                          (some-other-step {} {})))))
-  (testing "that the result of a step is passed on to the next step"
-    (is (= {:status :success :foo :baz} (chain
-                                          (some-step {} {})
-                                          (some-other-step {} {})))))
   (testing "that a failing step stops the execution"
     (is (= {:status :failure :foo :bar} (chain
                                           (some-step {} {})
