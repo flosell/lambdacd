1b47113bcd62a2e1c776806ccd75018802963f1a
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-09-02 13:01:38 +0200

    add the first step of the getting started tutorial

--- js/creative.js
+++ js/creative.js
@@ -28,7 +28,7 @@
     });
 
     // Fit Text Plugin for Main Header
-    $("h1").fitText(
+    $("header h1").fitText(
         1.2, {
             minFontSize: '35px',
             maxFontSize: '65px'
