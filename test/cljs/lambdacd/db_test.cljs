67836e016b07f45aa70f7611b219490c6e8bcc9e
Florian Sellmayr <florian.sellmayr@gmail.com>
2015-09-20 20:32:33 +0200

    merge tests for getting, setting history

--- test/cljs/lambdacd/db_test.cljs
+++ test/cljs/lambdacd/db_test.cljs
@@ -1,20 +1,18 @@
 (ns lambdacd.db-test
-  (:require [cemerick.cljs.test :refer-macros [is are deftest testing use-fixtures done]]
-            [reagent.core :as r]
-            [lambdacd.db :as db]))
+  (:require-macros
+    [cemerick.cljs.test :refer [is are deftest testing use-fixtures done]])
+  (:require
+    [reagent.core :as r]
+    [lambdacd.db :as db]))
 
-(deftest history-updated-handler-test
-         (testing "that we can update the history"
-                  (let [db { :history [:some :history]}]
-                    (is (= { :history [:some :other-history]} (db/history-updated-handler db [nil [:some :other-history]]))))))
-
-
-(deftest history-subscription-test
-         (testing "that we can get the history out"
-                  (let [db (r/atom { :history [:some :history]})]
+(deftest get-set-history-test
+         (testing "that we can set and update history"
+                  (let [db (r/atom {})]
+                    (reset! db (db/history-updated-handler @db [nil [:some :history]]))
                     (is (= [:some :history] @(db/history-subscription db nil))))))
 
-(deftest state-subscription-test
-         (testing "that we can get the state out"
-                  (let [db (r/atom { :pipeline-state [:some :state]})]
+(deftest get-set-pipeline-state-test
+         (testing "that we can set and update history"
+                  (let [db (r/atom {})]
+                    (reset! db (db/pipeline-state-updated-handler @db [nil [:some :state]]))
                     (is (= [:some :state] @(db/pipeline-state-subscription db nil))))))
