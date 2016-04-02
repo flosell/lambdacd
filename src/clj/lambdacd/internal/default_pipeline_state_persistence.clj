7bad8349a10fe76c1fdbfe0295d387a07fc4145e
Florian Sellmayr <florian.sellmayr@gmail.com>
2016-04-02 15:52:33 +0200

    change persistence format to edn to be able to support all basic data types in step results (#101)

--- src/clj/lambdacd/internal/default_pipeline_state_persistence.clj
+++ src/clj/lambdacd/internal/default_pipeline_state_persistence.clj
@@ -11,26 +11,39 @@
             [clj-time.coerce :as c]
             [cheshire.core :as ch]
             [cheshire.generate :as chg]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.edn :as edn]
+            [clojure.walk :as walk]))
 
+(defn convert-if-instance [c f]
+  (fn [x]
+    (if (instance? c x)
+      (f x)
+      x)))
+
+(defn clj-times->dates [m]
+  (walk/postwalk (convert-if-instance DateTime c/to-date) m))
+
+(defn dates->clj-times [m]
+  (walk/postwalk (convert-if-instance Date c/to-date-time) m))
 
 (defn- formatted-step-id [step-id]
-  (str/join "-" step-id ))
+  (str/join "-" step-id))
 
 (defn- unformat-step-id [formatted-step-id]
   (map util/parse-int (str/split formatted-step-id (Pattern/compile "-"))))
 
-(defn- step-result->json-format [[k v]]
+(defn- step-result->step-result-with-formatted-step-ids [[k v]]
   {:step-id (formatted-step-id k) :step-result v})
 
-(defn- pipeline-state->json-format [pipeline-state]
-  (map step-result->json-format pipeline-state))
+(defn- pipeline-state->formatted-step-ids [pipeline-state]
+  (map step-result->step-result-with-formatted-step-ids pipeline-state))
 
-(defn- step-json->step [{step-result :step-result step-id :step-id}]
+(defn- step-result-with-formatted-step-ids->step-result [{step-result :step-result step-id :step-id}]
   {(unformat-step-id step-id) step-result})
 
-(defn- json-format->pipeline-state [json-map]
-  (into {} (map step-json->step json-map)))
+(defn- formatted-step-ids->pipeline-state [m]
+  (into {} (map step-result-with-formatted-step-ids->step-result m)))
 
 
 (defn- to-date-if-date [v]
@@ -46,29 +59,67 @@
 (defn- build-number-from-path [path]
   (util/parse-int (second (re-find #"build-(\d+)" path))))
 
-(defn- read-build [path]
+(defn- read-build-json [path]
   (let [build-number (build-number-from-path path)
-        state        (json-format->pipeline-state (json/read-str (slurp path) :key-fn keyword :value-fn post-process-values))]
+        state        (formatted-step-ids->pipeline-state (json/read-str (slurp path) :key-fn keyword :value-fn post-process-values))]
     {build-number state}))
 
-(defn- write-build [path build]
-  (let [state-as-json        (pipeline-state->json-format build)
+(defn- read-build-edn [path]
+  (let [build-number (build-number-from-path path)
+        data-str     (slurp path)
+        state        (formatted-step-ids->pipeline-state (dates->clj-times (edn/read-string data-str)))]
+    {build-number state}))
+
+(defn- write-build-json [path build]
+  (let [state-as-json        (pipeline-state->formatted-step-ids build)
         state-as-json-string (util/to-json state-as-json)]
     (spit path state-as-json-string)))
 
-(defn write-build-history [home-dir build-number new-state]
+(defn- write-build-edn [path build]
+  (let [serializable-build  (clj-times->dates (pipeline-state->formatted-step-ids build))
+        state-as-edn-string (pr-str serializable-build)]
+    (spit path state-as-edn-string)))
+
+(defn- build-state-filename [json-or-edn]
+  (if (= :json json-or-edn)
+    "pipeline-state.json"
+    "build-state.edn"))
+
+(defn- build-state-path [dir json-or-edn]
+  (str dir "/" (build-state-filename json-or-edn)))
+
+(defn write-build-history-internal [home-dir build-number new-state json-or-edn]
   (if home-dir
-    (let [dir   (str home-dir "/" "build-" build-number "/")
-          path  (str dir "pipeline-state.json")
+    (let [dir   (str home-dir "/" "build-" build-number)
+          edn-path  (build-state-path dir :edn)
+          json-path (build-state-path dir :json)
           build (get new-state build-number)]
       (.mkdirs (io/file dir))
-      (write-build path build))))
+      (if (= :json json-or-edn)
+        (write-build-json json-path build)
+        (do
+          (write-build-edn edn-path build)
+          (write-build-json json-path build))))))
 
-(defn read-build-history-from [home-dir]
+(defn read-build-edn-or-json [dir json-or-edn]
+  (let [edn-path  (build-state-path dir :edn)
+        json-path (build-state-path dir :json)]
+    (if (and
+          (.exists (io/file edn-path))
+          (= :prefer-edn json-or-edn))
+      (read-build-edn edn-path)
+      (read-build-json json-path))))
+
+(defn read-build-history-from-internal [home-dir json-or-edn]
   (let [dir                 (io/file home-dir)
         home-contents       (file-seq dir)
         directories-in-home (filter #(.isDirectory %) home-contents)
         build-dirs          (filter #(.startsWith (.getName %) "build-") directories-in-home)
-        build-state-files   (map #(str % "/pipeline-state.json") build-dirs)
-        states              (map read-build build-state-files)]
+        states              (map #(read-build-edn-or-json % json-or-edn) build-dirs)]
     (into {} states)))
+
+(defn write-build-history [home-dir build-number new-state]
+  (write-build-history-internal home-dir build-number new-state :prefer-edn))
+
+(defn read-build-history-from [home-dir]
+  (read-build-history-from-internal home-dir :prefer-edn))
--- test/clj/lambdacd/internal/default_pipeline_state_persistence_test.clj
+++ test/clj/lambdacd/internal/default_pipeline_state_persistence_test.clj
@@ -6,8 +6,54 @@
             [lambdacd.util :as utils]))
 
 (deftest roundtrip-persistence-test
-  (let [some-pipeline-state { 3 {'(0)     {:status :success :most-recent-update-at (t/epoch)}
-                                 '(0 1 2) {:status :failure :out "something went wrong"}}}
-        home-dir (utils/create-temp-dir)]
-    (write-build-history home-dir 3 some-pipeline-state)
-    (is (= some-pipeline-state (read-build-history-from home-dir)))))
+  (testing "the standard case"
+    (let [some-pipeline-state {3 {'(0)     {:status :success :most-recent-update-at (t/epoch)}
+                                  '(0 1 2) {:status :failure :out "something went wrong"}}}
+          home-dir            (utils/create-temp-dir)]
+      (write-build-history home-dir 3 some-pipeline-state)
+      (is (= some-pipeline-state (read-build-history-from home-dir)))))
+  (testing "that string-keys in a step result are suppored as well (#101)"
+    (let [some-pipeline-state {3 {'(0) {:status :success :_git-last-seen-revisions {"refs/heads/master" "some-sha"}}}}
+          home-dir            (utils/create-temp-dir)]
+      (write-build-history home-dir 3 some-pipeline-state)
+      (is (= some-pipeline-state (read-build-history-from home-dir)))))
+  (testing "that keyworded values in a step result are suppored as well (#101)"
+    (let [some-pipeline-state {3 {'(0) {:status :success :v :x}}}
+          home-dir            (utils/create-temp-dir)]
+      (write-build-history home-dir 3 some-pipeline-state)
+      (is (= some-pipeline-state (read-build-history-from home-dir)))))
+  (testing "backwards compatibility"
+    (testing "that old json is read if no edn file exists"
+      (let [some-pipeline-state {3 {'(0) {:status :success :source "json"}}}
+            home-dir            (utils/create-temp-dir)]
+        (write-build-history-internal home-dir 3 some-pipeline-state :json)
+        (is (= some-pipeline-state (read-build-history-from home-dir)))))
+    (testing "that edn is preferred if json and edn exist"
+      (let [json-state {3 {'(0) {:status :success :source "json"}}}
+            edn-state  {3 {'(0) {:status :success :source "edn"}}}
+            home-dir   (utils/create-temp-dir)]
+        (write-build-history-internal home-dir 3 json-state :json)
+        (write-build-history-internal home-dir 3 edn-state :edn)
+        (is (= edn-state (read-build-history-from home-dir)))))
+    (testing "that both json and edn are written so that users can still return to earlier versions without losing history"
+      (let [some-pipeline-state {3 {'(0) {:status :success :source "json"}}}
+            home-dir            (utils/create-temp-dir)]
+        (write-build-history home-dir 3 some-pipeline-state)
+        (is (= some-pipeline-state (read-build-history-from home-dir)))
+        (is (= some-pipeline-state (read-build-history-from-internal home-dir :json)))))))
+
+(defn- roundtrip-date-time [data]
+  (dates->clj-times
+    (clj-times->dates data)))
+
+(deftest date-time-roundtrip-test
+  (testing "a simple case"
+    (let [data {:now (t/now)}]
+      (is (= data (roundtrip-date-time data)))))
+  (testing "vectors"
+    (let [data [(t/now)]]
+      (is (= data (roundtrip-date-time data)))))
+  (testing "nested maps"
+    (let [data {:now (t/now)
+                :other-times {"epoch" (t/epoch)}}]
+      (is (= data (roundtrip-date-time data))))))
