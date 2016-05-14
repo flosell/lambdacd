09345e7347d9dc5f1aebe91b349592dc560000d2
Florian Sellmayr <florian.sellmayr@gmail.com>
2016-05-14 20:47:56 +0200

    add basic example on how to test a pipeline

---
+++ test/clj/lambdacd/example/pipeline_test.clj
@@ -0,0 +1,17 @@
+(ns lambdacd.example.steps-test
+  (:require [clojure.test :refer [deftest testing is]]
+            [conjure.core :as c]
+            [lambdacd.steps.git :as git]
+            [todopipeline.steps :as steps]
+            [todopipeline.pipeline :as pipeline]
+            [lambdacd.steps.shell :as shell]
+            [lambdacd.internal.execution :as execution]
+            [lambdacd.testsupport.data :refer [some-ctx]]))
+
+(deftest todopipeline-test
+  (testing "a successful pipeline run" ; this isn't a particularly interesting test but it's a start
+    (c/stubbing [git/wait-with-details {:status :success}
+                 shell/bash {:status :success
+                             :exit 0}]
+                (is (= :success (:status (execution/run pipeline/pipeline-def (some-ctx)))))
+                (is (c/verify-called-once-with-args shell/bash )))))
