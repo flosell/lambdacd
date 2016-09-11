c4100fb9859e3c89fe273f01e6d8e2b188f3e244
Florian Sellmayr <florian.sellmayr@gmail.com>
2016-09-11 11:30:07 +0200

    Add sleep (shudder) to try and make event-bus-test less flaky

--- test/clj/lambdacd/event_bus_test.clj
+++ test/clj/lambdacd/event_bus_test.clj
@@ -6,14 +6,18 @@
             [lambdacd.testsupport.data :refer :all]
             [clojure.core.async :as async]))
 
+; TODO: subscribe and unsubscribe are async and I haven't figured out how to wait for subscription to really be ready.
+; Sleeping after such operations to make the test is less flaky
 
 (deftest event-bus-test
   (testing "that we can publish and subscribe to events"
     (let [ctx (initialize-event-bus (some-ctx))]
       (let [subscription (subscribe ctx :test-messages)]
+        (Thread/sleep 100)
         (publish!!   ctx :test-messages {:message-number 1})
         (publish!! ctx :other-topic {:other-message "hello"})
         (unsubscribe ctx :test-messages subscription)
+        (Thread/sleep 100)
         (publish!! ctx :test-messages {:message-number 2})
         (is (= [{:message-number 1}] (slurp-chan-with-size 1 (only-payload subscription)))))))
   (testing "that messages get delivered to all subscribers if more than one subscribes to the same topic"
@@ -22,6 +26,7 @@
           subscription-2 (subscribe ctx :test-messages)
           payloads-1 (buffered (only-payload subscription-1))
           payloads-2 (buffered (only-payload subscription-2))]
+      (Thread/sleep 100)
       (publish!! ctx :test-messages {:message-number 1})
       (publish!! ctx :test-messages {:message-number 2})
 
