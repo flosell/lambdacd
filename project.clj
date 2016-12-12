13c0b218c4afaf7e62a21c9cb2ee51280fc92247
Florian Sellmayr <florian.sellmayr@gmail.com>
2016-12-12 15:30:18 +0100

    Fix upcoming release version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambdacd "0.11.1-SNAPSHOT"
+(defproject lambdacd "0.12.0-SNAPSHOT"
   :description "a library to create a continous delivery pipeline in code"
   :url "http://github.com/flosell/lambdacd"
   :license {:name "Apache License, version 2.0"
