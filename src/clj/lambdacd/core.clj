a9e0c8c42a87cf0222c6d08058e3df5c95fb6a89
Florian Sellmayr <florian.sellmayr@gmail.com>
2017-01-31 22:28:20 +0100

    API Documentation for lambdacd.core

--- src/clj/lambdacd/core.clj
+++ src/clj/lambdacd/core.clj
@@ -1,14 +1,12 @@
 (ns lambdacd.core
+  "Main entrypoinint into LambdaCD. Provides core functionality to assemble an instance of LambdaCD that can run."
   (:use compojure.core)
   (:require [lambdacd.internal.default-pipeline-state :as default-pipeline-state]
-            [lambdacd.execution.core :as execution]
             [lambdacd.event-bus :as event-bus]
             [lambdacd.internal.running-builds-tracking :as running-builds-tracking]
             [lambdacd.state.internal.pipeline-state-updater :as pipeline-state-updater]
-            [lambdacd.execution.internal.execute-step :as execute-step]
             [clojure.tools.logging :as log]
             [lambdacd.runners :as runners]
-            [lambdacd.execution.internal.execute-steps :as execute-steps]
             [lambdacd.execution.internal.kill :as kill]
             [lambdacd.execution.core :as execution-core]))
 
@@ -19,13 +17,14 @@
                                  ((:shutdown-sequence (:config ctx)) ctx)))))
   ctx)
 
-(def default-shutdown-sequence
-  (fn [ctx]
-    (runners/stop-runner ctx)
-    (kill/kill-all-pipelines ctx)
-    (pipeline-state-updater/stop-pipeline-state-updater ctx)))
+(defn default-shutdown-sequence
+  "Default behavior when shutting down a Pipeline. Kills runnning pipelines, stops runners and internal processes."
+  [ctx]
+  (runners/stop-runner ctx)
+  (kill/kill-all-pipelines ctx)
+  (pipeline-state-updater/stop-pipeline-state-updater ctx))
 
-(def default-config
+(def ^{:doc "Default configuration if none other is specified"} default-config
   {:ms-to-wait-for-shutdown (* 10 1000)
    :shutdown-sequence default-shutdown-sequence
    :step-updates-per-sec 10
@@ -36,6 +35,9 @@
     (assoc ctx :pipeline-state-updater updater)))
 
 (defn assemble-pipeline
+  "Assemble various internal LambdaCD components into a unit ready to run.
+
+  Returns a map that contains :context and :pipeline-def"
   ([pipeline-def config]
    (assemble-pipeline pipeline-def config (default-pipeline-state/new-default-pipeline-state config)))
   ([pipeline-def config pipeline-state-component]
@@ -51,21 +53,26 @@
 
 (defn retrigger
   "DEPRECATED, use lambdacd.execution.core/retrigger instead"
+  {:deprecated "0.9.5"}
   [pipeline context build-number step-id-to-retrigger]
   (execution-core/retrigger-pipeline-async pipeline context build-number step-id-to-retrigger))
 
-(defn kill-step [ctx build-number step-id]
+(defn kill-step
   "DEPRECATED, use lambdacd.execution.core/kill-step instead"
+  {:deprecated "0.9.5"}
+  [ctx build-number step-id]
   (execution-core/kill-step ctx build-number step-id))
 
-(defn execute-steps [steps args ctx & opts]
+(defn execute-steps
   "DEPRECATED, use lambdacd.execution.core instead"
+  {:deprecated "0.9.5"}
+  [steps args ctx & opts]
   (apply execution-core/execute-steps steps args ctx opts))
 
 (defn execute-step
-  ([args ctx step]
    "DEPRECATED, use lambdacd.execution.core instead"
+   {:deprecated "0.9.5"}
+  ([args ctx step]
    (execution-core/execute-step args [ctx step]))
   ([args [ctx step]]
-   "DEPRECATED, use lambdacd.execution.core instead"
    (execution-core/execute-step args [ctx step])))
