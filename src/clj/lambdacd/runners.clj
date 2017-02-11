06f25a03f764f711d802b6130cf39a81981c0210
Florian Sellmayr <florian.sellmayr@gmail.com>
2017-02-11 17:48:17 +0100

    Document lambdacd.runners

--- src/clj/lambdacd/runners.clj
+++ src/clj/lambdacd/runners.clj
@@ -1,4 +1,6 @@
 (ns lambdacd.runners
+  "Runners are what keeps a pipeline going. The start new builds based on some logic,
+   e.g. when the previous build is finished or (e.g if the first step is a trigger) after the first step is done."
   (:require [lambdacd.execution.core :as execution]
             [clojure.core.async :as async]
             [lambdacd.event-bus :as event-bus]
@@ -17,7 +19,9 @@
   should-trigger-next-build?
   (every-pred is-first-step? is-not-retriggered?))
 
-(defmacro while-not-stopped [ctx & body]
+(defmacro while-not-stopped
+  "Execute a runners body until it is stopped from the outside (usually by someone calling `stop-runner`, e.g. on shutdown)."
+  [ctx & body]
   `(let [running# (atom true)]
      (async/go
        (async/<! (event-bus/subscribe ~ctx :stop-runner))
@@ -27,7 +31,7 @@
      (log/info "Runner stopped. No new pipeline instances will be spawned.")))
 
 (defn start-new-run-after-first-step-finished
-  "Runner that makes sure there is always one pipeline-run with the first step active. useful if the first step is waiting for
+  "Runner that makes sure there is always one pipeline-run with the first step active. Useful if the first step is waiting for
   a trigger such as a VCS commit and you don't want to wait for the whole pipeline to complete before starting the next run.
   Do not use this if your first step finishes immediately as this will lead to lots of active pipelines"
   [{pipeline-def :pipeline-def context :context}]
@@ -41,12 +45,16 @@
         (async/<!! first-steps-finisehd)))))
 
 (defn start-one-run-after-another
-  "Runner that always keeps one pipeline-run active. It waits for a run to complete, then starts a new one."
+  "Runner that always keeps one pipeline-run active. It waits for a run to complete, then starts a new one.
+  Commonly used when running multiple pipelines in parallel is not an option or the pipeline has no trigger."
   [{pipeline-def :pipeline-def context :context}]
  (async/thread
    (while-not-stopped context
      (execution/run-pipeline pipeline-def context))))
 
-(defn stop-runner [ctx]
+(defn stop-runner
+  "Triggers an event that will stop runners so they no longer start new pipeline-runs.
+  Does not stop running pipelines and does not wait for runners to stop."
+  [ctx]
   (log/info "Stopping runner...")
   (event-bus/publish!! ctx :stop-runner {}))
