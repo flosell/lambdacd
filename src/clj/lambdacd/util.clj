d01954d51f2deee0282f45866440eb0ef0a08410
Florian Sellmayr <florian.sellmayr@gmail.com>
2017-02-13 22:43:05 +0100

    Add deprecation metadata on lambdacd.util

--- src/clj/lambdacd/util.clj
+++ src/clj/lambdacd/util.clj
@@ -17,81 +17,98 @@
 
 (defn no-file-attributes
   "DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   []
   (into-array FileAttribute []))
 
 
-(def temp-prefix temp-utils/temp-prefix) ; DEPRECATED, WILL BE REMOVED SOON
+(def ^{:doc "DEPRECATED, WILL BE REMOVED SOON"
+       :deprecated "0.12.1"} temp-prefix temp-utils/temp-prefix) ; DEPRECATED, WILL BE REMOVED SOON
 
 (defn create-temp-dir
   "DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   ([]
     (temp-utils/create-temp-dir))
   ([parent]
    (temp-utils/create-temp-dir parent)))
 
 
-(defn create-temp-file []
+(defn create-temp-file
   "DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
+  []
   (temp-utils/create-temp-file))
 
 (defmacro with-temp
   "DEPRECATED, WILL BE REMOVED SOON
   evaluates the body, then deletes the given file or directory.
   returns the result of the evaluation of the body"
+  {:deprecated "0.12.1"}
   [f body]
   `(temp-utils/with-temp ~f ~body))
 
 
 (defn write-as-json
   "UNUSED AND DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   [file data]
   (spit file (json/write-str data)))
 
 (defn bash
   "DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   [cwd & commands]
   (apply bash-utils/bash cwd commands))
 
 
-(defn map-if [pred f coll]
+(defn map-if
   "UNUSED AND DEPRECATED, WILL BE REMOVED SOON" ; only used by test
+  {:deprecated "0.12.1"}
+  [pred f coll]
   (map #(if (pred %)
          (f %)
          %) coll))
 
 (defn put-if-not-present
   "DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   [m k v]
   (map-utils/put-if-not-present m k v))
 
 (defn parse-int
   "DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   [int-str]
   (sugar-utils/parse-int int-str))
 
 (defn contains-value?
   "DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   [v coll]
   (map-utils/contains-value? v coll))
 
 (defn to-json
   "DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   [v]
   (ui-utils/to-json v))
 
 (defn buffered
   "DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   [ch]
   (async-utils/buffered ch))
 
 (defn json
   "DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   [data]
   (ui-utils/json data))
 
 (defn ok
   "UNUSED AND DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   []
   {:headers { "Content-Type" "application/json"}
    :body "{}"
@@ -99,10 +116,12 @@
 
 (defn fill
   "DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   [coll length filler]
   (coll-utils/fill coll length filler))
 
 (defn merge-with-k-v
   "DEPRECATED, WILL BE REMOVED SOON"
+  {:deprecated "0.12.1"}
   [f & maps]
   (apply map-utils/merge-with-k-v f maps))
