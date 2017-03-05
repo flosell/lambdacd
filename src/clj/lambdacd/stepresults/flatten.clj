a73ad7506e5f3f6bbd5ea7387d154ae815933343
Florian Sellmayr <florian.sellmayr@gmail.com>
2017-03-05 12:54:26 +0100

    Document flatten-step-result-outputs

--- src/clj/lambdacd/stepresults/flatten.clj
+++ src/clj/lambdacd/stepresults/flatten.clj
@@ -1,6 +1,19 @@
-(ns lambdacd.stepresults.flatten)
+(ns lambdacd.stepresults.flatten
+  "Functions to convert a nested step result and transform it into a flat list")
 
-(defn flatten-step-result-outputs [outputs]
+(defn flatten-step-result-outputs
+  "Takes a nested step-result map (like those returned by a step or sent in a:pipeline-finished event)
+  and converts it into a flat map of step results where every step is accessible with its step-id:
+  ```
+  > (flatten-step-result-outputs {[1] {:status :success}
+                                  [2] {:status  :success
+                                       :outputs {[1 2] {:status :success :step [1 2]}}}})
+  {[1]   {:status :success}
+   [2]   {:status  :success
+          :outputs {[1 2] {:status :success :step [1 2]}}}
+   [1 2] {:status :success :step [1 2]}}
+  ```"
+  [outputs]
   (into {}
         (for [[k v] outputs]
           (if (:outputs v)
